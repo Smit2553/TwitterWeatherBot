32868e046d147ac05eb8aa65302c0505249a7b67
Smit <107775677+Smit2553@users.noreply.github.com>
2022-08-07 17:43:00 -0700

    Final 08/07/2022

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-import requests
-from dotenv import load_dotenv
-import os
 from weather import *
+import tweepy
+import urllib.parse
+import time
 
 
-def twitterpost(data=get_weather_data()):
+def twitterpost():
+    city = get_city()
+    data = get_weather_data(city)
     temperature_k = data['main']['temp']
-    temperature_c = temperature_k - 273.15
-    temperature_f = 1.8 * (temperature_k - 273) + 32
+    temperature_c = round((temperature_k - 273.15), 2)
+    temperature_f = round((1.8 * (temperature_k - 273) + 32), 2)
     general_weather = data['weather'][0]['description']
+    consumer_key = os.environ.get('CONSUMERKEY')
+    consumer_secret = os.environ.get('CONSUMERSECRET')
+    access_token = os.environ.get('ACCESSTOKEN')
+    access_token_secret = os.environ.get('ACCESSTOKENSECRET')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    googlemapslink = urllib.parse.quote(f'{city[0]}')
+
+    api.update_status(status=f"""
+    Location = {city[0]}, {city[1]}
+    Celcius = {temperature_c} C
+    Fahrenheit = {temperature_f} F
+    Conditions = {general_weather}
+    https://www.google.com/maps/search/?api=1&query={googlemapslink}  #weather #city #{city[0]}""")
+    print(f'Location = {city[0]}, {city[1]}')
+
+
+while True:
+    twitterpost()
+    time.sleep(3600)
--- weather.py
+++ weather.py
@@ -6,10 +6,7 @@ from city import *
 load_dotenv()
 
 
-def get_weather_data(city=None):
-    if city is None:
-        city = get_city()
-
+def get_weather_data(city):
     weatherapikey = os.environ.get("WEATHERAPI")
     response = requests.get(
         f'http://api.openweathermap.org/data/2.5/weather?q={city[0]},{city[1]}&APPID={weatherapikey}').json()
