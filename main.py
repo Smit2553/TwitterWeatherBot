ebdd7ac4f4c64cd0605a33d21de1a981d80ce694
Hemanti S D <95442976+HemantiSD@users.noreply.github.com>
2022-08-19 11:16:11 -0700

    Fixed crashes

--- main.py
+++ main.py
@@ -5,35 +5,37 @@ import time
 
 
 def twitterpost():
-    city = get_city()
-    data = get_weather_data(city)
-    temperature_k = data['main']['temp']
-    temperature_c = round((temperature_k - 273.15), 2)
-    temperature_f = round((1.8 * (temperature_k - 273) + 32), 2)
-    general_weather = data['weather'][0]['description']
-    consumer_key = os.environ.get('CONSUMERKEY')
-    consumer_secret = os.environ.get('CONSUMERSECRET')
-    access_token = os.environ.get('ACCESSTOKEN')
-    access_token_secret = os.environ.get('ACCESSTOKENSECRET')
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    googlemapslink = urllib.parse.quote(f'{city[0]}')
+    try:
+        city = get_city()
+        data = get_weather_data(city)
+        temperature_k = data['main']['temp']
+        temperature_c = round((temperature_k - 273.15), 2)
+        temperature_f = round((1.8 * (temperature_k - 273) + 32), 2)
+        general_weather = data['weather'][0]['description']
+        consumer_key = os.environ.get('CONSUMERKEY')
+        consumer_secret = os.environ.get('CONSUMERSECRET')
+        access_token = os.environ.get('ACCESSTOKEN')
+        access_token_secret = os.environ.get('ACCESSTOKENSECRET')
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        googlemapslink = urllib.parse.quote(f'{city[0]}')
 
-    api.update_status(status=f"""
-    Location = {city[0]}, {city[1]}
-    Celcius = {temperature_c} C
-    Fahrenheit = {temperature_f} F
-    Conditions = {general_weather}
-    https://www.google.com/maps/search/?api=1&query={googlemapslink}  #weather #city #{city[0].replace(" ", "")}""")
-    print(f'Location = {city[0]}, {city[1]}')
+        api.update_status(status=f"""
+        Location = {city[0]}, {city[1]}
+        Celcius = {temperature_c} C
+        Fahrenheit = {temperature_f} F
+        Conditions = {general_weather}
+        https://www.google.com/maps/search/?api=1&query={googlemapslink}  #weather #city #{city[0].replace(" ", "")}""")
+        print(f'Location = {city[0]}, {city[1]}')
+    except:
+        print('Error')
+        time.sleep(60)
+        twitterpost()
 
 
 def main():
-    try:
-        twitterpost()
-    except:
-        twitterpost()
+    twitterpost()
 
 
 while True:
